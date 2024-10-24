18e8015d77ccc6c0c3b86a246c562d517a24b7d9
Romain Ruetschi <github@romac.me>
2024-10-24 16:38:48 +0200

    fish: Update GraalVM

--- fish/.config/fish/conf.d/java.fish
+++ fish/.config/fish/conf.d/java.fish
@@ -1,3 +1,6 @@
 # >>> JVM installed by coursier >>>
-set -gx JAVA_HOME "/Users/romac/Library/Caches/Coursier/arc/https/github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_macos-aarch64_bin.tar.gz/graalvm-community-openjdk-21.0.2+13.1/Contents/Home"
+set -gx JAVA_HOME "/Users/romac/Library/Caches/Coursier/arc/https/github.com/graalvm/graalvm-ce-builds/releases/download/jdk-23.0.1/graalvm-community-jdk-23.0.1_macos-aarch64_bin.tar.gz/graalvm-community-openjdk-23.0.1+11.1/Contents/Home"
 # <<< JVM installed by coursier <<<
+
+fish_add_path -a "$HOME/Library/Application Support/Coursier/bin"
+fish_add_path -a "$JAVA_HOME/bin"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,10 @@
 # Disable question mark globbing
 # set -U fish_features qmark-noglob
 
-
-
 # Path
 fish_add_path -a "$HOME/.bin"
 fish_add_path -a "$HOME/.cargo/bin"
-fish_add_path -a "$JAVA_HOME/bin"
 fish_add_path -a "$HOME/.bun/bin"
-fish_add_path -a "$HOME/Library/Application Support/Coursier/bin"
 fish_add_path -a "$HOME/.gm/bin"
 fish_add_path -a "$HOME/.local/bin"
 fish_add_path -a "/opt/homebrew/bin"
@@ -33,4 +29,3 @@ source "$HOME/.dotfiles/env/private.env"
 #         printf "%s\n" $targets\t"$description"
 #     end
 # end
-
